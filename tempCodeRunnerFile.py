6e87d215473a05e653c9ee9c443f8909a51c0916
HuangTingWei <0978wally@gmail.com>
2024-07-19 13:57:21 +0800

    .

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
- if cubeType == 'LARGE':
-        move_rel(s,'PTP',(0,0,-60,0,0,0)) #放下夾爪
-        move_rel(s,'PTP',(0,0,60,0,0,0)) #抬起夾爪
-    elif cubeType[j] == 'MID':
-        move_rel(s,'PTP',(0,0,-60,0,0,0)) #放下夾爪
-        move_rel(s,'PTP',(0,0,60,0,0,0)) #抬起夾爪
-    elif cubeType[j] == 'SMALL':
-        move_rel(s,'PTP',(0,0,-60,0,0,0)) #放下夾爪
-        move_rel(s,'PTP',(0,0,60,0,0,0)) #抬起夾爪
